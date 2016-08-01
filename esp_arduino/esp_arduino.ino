93e0faf3944163e01118d27dff4d52d7bbc99445
metalheart <maksym.antipov@gmail.com>
2016-08-01 09:44:12 +0300

    added message exchange

--- esp_arduino/esp_arduino.ino
+++ esp_arduino/esp_arduino.ino
@@ -21,6 +21,24 @@ struct eeprom_data_t {
   bool configured;
 } eeprom_data;
 
+#define MAX_SCAN_TASKS 16
+#define SCAN_TASK_TAG_LEN 14
+
+enum task_status_t {
+  TS_NEW = 1,
+  TS_COMPLETED = 1 << 1
+};
+
+typedef struct scan_task {
+  byte tag[SCAN_TASK_TAG_LEN];
+  task_status_t status;
+} scan_task_t;
+
+typedef struct scan_tasks {
+  scan_task_t tasks[MAX_SCAN_TASKS];
+  byte num_tasks;
+} scan_tasks_t;
+
 static PROGMEM prog_uint32_t crc_table[16] = {
   0x00000000, 0x1db71064, 0x3b6e20c8, 0x26d930ac, 0x76dc4190, 0x6b6b51f4, 0x4db26158, 0x5005713c,
   0xedb88320, 0xf00f9344, 0xd6d6a3e8, 0xcb61b38c, 0x9b64c2b0, 0x86d3d2d4, 0xa00ae278, 0xbdbdf21c
@@ -106,10 +124,6 @@ int base64_enc_len(int plainLen) {
 /**/
 
 // ----------------------------------- setup -----------------------------------
-
-WiFiServer server(12345);
-StaticJsonBuffer<512> jsonBuffer;
-
 void setupSettings(const char *ssid, const char *pwd, long long serial) {
   strcpy(eeprom_data.ssid, ssid);
   strcpy(eeprom_data.pwd, pwd);
@@ -168,12 +182,17 @@ bool readSettings() {
   }
 }
 
+
+WiFiServer server(12345);
+
+StaticJsonBuffer<512> jsonBuffer;
+
 bool doBinding() {
   WiFi.begin("Rescan Bind AP", "\"123456789\"");
   
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
+    Serial.print("]");
   }
   Serial.println("");
   Serial.println("WiFi connected");
@@ -188,29 +207,26 @@ bool doBinding() {
   WiFiClient client;
   do {
     client = server.available();
-    Serial.print(".");
+    Serial.print("]");
     delay(500);
   } while (!client);
  
   // Wait until the client sends some data
   Serial.println("new client");
-  //while(!client.available()){
-  //  delay(1);
-  //}
-
+  
   JsonObject& root = jsonBuffer.createObject();
   root["id"] = s_hw_serial_number;
   root["version"] = s_hw_version;
   root.printTo(client);
   client.flush();
-  
-  client.flush();
   String response = client.readStringUntil('\r');
   
   
   JsonObject& object = jsonBuffer.parseObject(response);
+  
   const char* ssid = object["SSID"];
   const char* pwd = object["PWD"];
+  
   Serial.println(ssid);
   Serial.println(pwd);
   if (strlen(ssid) > 0 && strlen(pwd) > 0) {
@@ -222,54 +238,59 @@ bool doBinding() {
 
 String IpAddress2String(const IPAddress& ipAddress)
 {
-  return String(ipAddress[0]) + String(".") +\
-  String(ipAddress[1]) + String(".") +\
-  String(ipAddress[2]) + String(".") +\
-  String(ipAddress[3])  ; 
+  return String(ipAddress[0]) + String(".")
+    + String(ipAddress[1]) + String(".")
+    + String(ipAddress[2]) + String(".")
+    + String(ipAddress[3]); 
 }
 
-WiFiUDP udp;
 bool startAdvertising(WiFiClient& client) {
-  WiFi.begin(eeprom_data.ssid, eeprom_data.pwd);
+  if (WiFi.status() != WL_CONNECTED) {
+    WiFi.begin(eeprom_data.ssid, eeprom_data.pwd);
+  }
   
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
+    Serial.print("-");
   }
+  
   Serial.println("");
   Serial.println("WiFi connected");
 
   // Start the server
   server.begin();
-  Serial.println("Start advertising from ");
-  Serial.println(WiFi.localIP());
-  
-  udp.begin(666);
+
   IPAddress ip = WiFi.localIP();
   ip[3] = 255;
 
-  JsonObject& root = jsonBuffer.createObject();
-  root["id"] = s_hw_serial_number;
-  root["version"] = s_hw_version;
-  root["ip"] = IpAddress2String(WiFi.localIP());
-  String s;
-  root.printTo(s);
-    
+  Serial.println("Start advertising from ");
+  Serial.println(ip);
+
+  JsonObject& connectPkt = jsonBuffer.createObject();
+  connectPkt["id"] = s_hw_serial_number;
+  connectPkt["version"] = s_hw_version;
+  connectPkt["ip"] = IpAddress2String(WiFi.localIP());
+  
+  WiFiUDP udp;
+  
   do {
   // transmit broadcast package
     udp.beginPacket(ip, 6666);
-    udp.write(s.c_str());
+    connectPkt.printTo(udp);
     udp.endPacket();
-
-    client = server.available();
     delay(500);
-  } while (!client);
+    client = server.available();
+    
+    Serial.print("...");
+  } while (!client.connected());
 
+  udp.stopAll();
+  
   return true;
 }
 
-bool clientReady = false;
-
+WiFiClient client;
+  
 void setup() {
   EEPROM.begin(512);
   
@@ -287,41 +308,143 @@ void setup() {
 
   Serial.println("Configured!!!");
   server.begin();
-  WiFiClient client;
 
   if (!startAdvertising(client)) {
     
   }
-  
-  clientReady = true;
-  /*if (eeprom_data.STAenabled == true)
-  {
-    //WiFi.mode(WIFI_AP_STA);
-    WiFi.mode(WIFI_STA); // AP on STA timeout or STA connect
-    STAinit();
-  }
-  else
-  {
-    //WiFi.mode(m): set mode to WIFI_AP, WIFI_STA, or WIFI_AP_STA.
-    WiFi.mode(WIFI_AP);
-    softAPinit();
-
-  }
-
-
-
-
-  nextSensTick = millis();
-  nextSerialTick =  millis() ;
-  nextwifiCheckTick = millis() ;
-  nextSenderTick = 20000 + millis() ;*/
-
 }
 
 // ----------------------------------- loop -----------------------------------
-void loop() {
-  if (clientReady) {
-      delay(500);
-      Serial.println("!!!!");
+scan_tasks_t _scan_tasks;
+
+bool allTasksCompleted(const scan_tasks_t& tasks) {
+  
+  for (int i = 0; i < tasks.num_tasks; ++i) {
+      if (!(tasks.tasks[i].status & TS_COMPLETED)) {
+        return false;
+      }
   }
+
+  return true;
+}
+
+bool readyForNewTsak(const scan_tasks_t& tasks) {
+  return tasks.num_tasks == 0 || allTasksCompleted(tasks);
+}
+
+//static char taskBuffer[512];
+const char* readJsonSafe(WiFiClient& client) {
+  static char tmpBuff[512+1] = "";
+  
+  int available = client.available();
+  //Serial.print("readJsonSafe: available ");
+  //Serial.print(available);
+  //Serial.println(" bytes");
+  
+  if (available > 1) {
+    if (available >= 512) {
+      available = 512;
+    }
+    client.peekBytes(tmpBuff, available);
+    
+    int brackets = 0;
+    int msgLen = 0;
+    bool msgRead = false;
+    
+    while (msgLen < available && !msgRead) {
+      if (tmpBuff[msgLen] == '{') {
+        brackets += 1;
+      } else if (tmpBuff[msgLen] == '}') {
+        brackets -= 1;
+        if (brackets == 0) {
+          msgRead = true;
+          ++ msgLen;
+          break;
+        }
+      }
+      ++ msgLen;
+    }
+
+    if (brackets == 0 && msgRead) {
+      client.read((uint8_t*)tmpBuff, msgLen);
+      tmpBuff[msgLen] = 0;
+      return tmpBuff;
+    }
+  }
+
+  return NULL;
+}
+
+void loop() {
+  if (WiFi.status() == WL_CONNECTED && client.connected()) {
+
+    const char* szJsonStr = readJsonSafe(client);
+    
+    if (szJsonStr) {
+      //Serial.println(szJsonStr);
+      JsonObject& object = jsonBuffer.parseObject(szJsonStr);
+      if (object.success()) {
+        String type = object.get<String>("type");
+        if (type == "task")
+        {
+          if (readyForNewTsak(_scan_tasks)) {
+            //try to fetch new tasks from server
+            Serial.println("Waiting for messages...");
+            
+          
+            Serial.println("Incoming msg...");
+          
+            JsonArray& taskArray = object["tasks"].asArray();
+            int numTasks = taskArray.size() > MAX_SCAN_TASKS ? MAX_SCAN_TASKS : taskArray.size();
+            for (int i = 0; i < numTasks; ++i) {
+              const char *tag = taskArray.get<const char*>(i);
+              if (strlen(tag) == SCAN_TASK_TAG_LEN) {
+                Serial.print("Task read:");
+                Serial.println(tag);
+                
+                memcpy(_scan_tasks.tasks[i].tag, tag, SCAN_TASK_TAG_LEN);
+              } else {
+                //handle error
+              }
+              _scan_tasks.tasks[i].status = TS_NEW;
+            }
+            _scan_tasks.num_tasks = numTasks;
+      
+          }
+        }
+        else
+        {
+          client.println("{\"type\":\"pong\"}");
+        }
+      }
+    }
+    
+    //try to read scanned tag from serial
+    if (Serial.available() > 0) 
+    {
+      char tag[14+1];
+      for (int i = 0; i < 14; ++i) {
+        tag[i] = Serial.read();
+      }
+      tag[14] = 0;
+      Serial.flush();
+
+      Serial.print("RFID read:");
+      Serial.println(tag);
+
+      for (int i = 0; i < _scan_tasks.num_tasks; ++i) {
+        if (memcmp(tag, (const char*)_scan_tasks.tasks[i].tag, SCAN_TASK_TAG_LEN) ==0)
+        {
+          _scan_tasks.tasks[i].status = TS_COMPLETED;
+          Serial.print("Task completed!");
+        }
+      }
+
+      //send status over the network..
+    }
+  } else {
+    startAdvertising(client);
+  }
+
+  delay(100);
 }
